f91b7132ad318aaecc05af37e3aba93ae9904a68
Mingu Kang <kanopes429@gmail.com>
2026-09-23 10:51:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 66.47 ms, Memory: 26.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    answer = []
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    for i in arr:
+        if not answer or i != answer[-1]:
+            answer.append(i)            
+    return answer
+
+def solution(arr):
+    answer = []
+    
+    for i in range(len(arr)):
+        if i == 0 or answer[-1] != arr[i]:
+            answer.append(arr[i])
+            
+    return answer
